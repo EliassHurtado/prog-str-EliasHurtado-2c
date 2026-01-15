b8f6b0fc1b9af86ec07c41c0c1545e33193a5db2
obedhuher12 <oahh12345678@gmail.com>
2026-01-14 22:30:51 -0600

    chore: base actividad git merge withoutconflict

--- MergeWithOutConflict/src/Main.java
+++ MergeWithOutConflict/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("2 + 2 = 4");
-        System.out.println("5 * 3 = 15");
+        System.out.println("Semana 2");
+        System.out.println("Versión base");
     }
 }
