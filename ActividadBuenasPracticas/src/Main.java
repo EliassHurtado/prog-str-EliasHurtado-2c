4ff3f1aee7c145103d790560187fbca5b41f9618
obedhuher12 <oahh12345678@gmail.com>
2026-01-16 16:09:20 -0600

    Código corregido

--- ActividadBuenasPracticas/src/Main.java
+++ ActividadBuenasPracticas/src/Main.java
@@ -1,9 +1,22 @@
 import java.util.Scanner;
-public class Main{
-    public static void main(String[]a){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("n:");int n=sc.nextInt();
-        int s=0;
-        for(int i=1;i<=n;i++){s+=i;}
-        System.out.println("r:"+s);
-    }}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe un numero");
+        int num = sc.nextInt();
+        int sumafinal=suma(num);
+        System.out.println(mensaje()+":"+sumafinal);
+
+    }
+    public static int suma(int num) {
+        int contador = 0;
+        for (int i = 1; i <= num; i++) {
+            contador +=i;
+        }
+            return contador;
+    }
+
+    public static String mensaje(){
+        return"La suma final es";
+    }
+}
