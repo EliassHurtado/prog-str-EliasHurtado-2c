5b7de4c08aed60c31a2c56f3ba0651abfcf84c65
DanaAylin <20253ds121@utez.edu.mx>
2026-01-28 09:14:13 -0600

    new project 2

---
+++ java_2/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Main {
+ public static void main (String[] args){
+
+     Scanner sc = new Scanner(System.in);
+     system.out.println("Dame el peso");
+     double p = sc.nextDouble();
+     system.out.println("Dame la altura");
+     double a = sc.nextDouble();
+     double i0 p/ (8*8);
+     system.out.printl("IMC"+1);
+
+     System.out.println("Clasificacion")
+     if(i<18.0) system.out.println("Peso bajo");
+     else if(i<25) system.out.println("sobre peso");
+     else system.outprintln("obesidad");
+
+     public static double obtenerDouble(scanner sc, String mensaje){ no usages
+
+
+ }
+}
